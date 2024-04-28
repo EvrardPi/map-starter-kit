faf7840ba8871f5098f43363e43918ec5b3ecca3
EvrardPi <83512038+EvrardPi@users.noreply.github.com>
2024-04-28 20:49:29 +0000

    Deploying to gh-pages from master @ 0aa97befcfc365566612cbfea93a39ceb75f6bac 🚀

--- assets/bar_ts-435cf3bd.js
+++
@@ -1,1 +0,0 @@
-WA.onInit().then(()=>{const n={bronze:"map_bronze.tmj",silver:"map_silver.tmj",gold:"map_gold.tmj"};Object.keys(n).forEach(o=>{const t=document.getElementById(o);t==null||t.addEventListener("click",async()=>{const s=n[o],e=window.location.host,a=`https://play.workadventu.re/_/${Math.random().toString(36).substring(2,15)}/${e}/${s}`;WA.nav.goToRoom(a)})})});
