0aa97befcfc365566612cbfea93a39ceb75f6bac
Pierre EVRARD <evrardpierrefm@gmail.com>
2024-04-28 22:48:55 +0200

    last?

--- src/bar/bar.ts
+++ src/bar/bar.ts
@@ -10,10 +10,13 @@ WA.onInit().then(() => {
         button?.addEventListener('click', async () => {
             const mapName = mapButtons[key];
 
-            const host = window.location.host;
+            let host = window.location.host;
             const instanceId = Math.random().toString(36).substring(2, 15);
-            const url = `https://play.workadventu.re/_/${instanceId}/${host}/${mapName}`;
+            let url = `https://play.workadventu.re/_/${instanceId}/${host}/map-starter-kit/${mapName}`;
 
+            if (host === 'localhost') {
+                url = `https://play.workadventu.re/_/${instanceId}/${host}/${mapName}`;
+            }
             WA.nav.goToRoom(url);
             //WA.nav.goToRoom(`https://play.workadventu.re/_/ixta2r3w9p/2d8f-77-132-167-143.ngrok-free.app/${mapName}`);
         });
